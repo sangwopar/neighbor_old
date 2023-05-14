91d66ff91977ad55f5e8edbead82aa98c0719f3b
우병걸 <wbg030281@gmail.com>
2023-05-15 01:52:30 +0900

    우병걸/0515/로그인폼

---
+++ src/main/java/com/neighbor/market/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.neighbor.market.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String test(Model model){
+        model.addAttribute("test","LOGIN");
+        return "login";
+    }
+}
