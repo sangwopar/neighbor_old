c5b4dec0061cf730bbcf6b892bb8649bff679a8c
우병걸 <wbg030281@gmail.com>
2023-05-15 17:14:56 +0900

    우병걸/0515/회원가입폼

--- src/main/java/com/neighbor/market/controller/TestController.java
+++ src/main/java/com/neighbor/market/controller/TestController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
     @GetMapping("/")
-    public String test(Model model){
+    public String login(Model model){
         model.addAttribute("test","LOGIN");
         return "login";
     }
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
 }
