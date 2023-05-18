467450704d26dd11560af8caf495be6b4f534cbd
song <jefkdjsf2@naver.com>
2023-05-18 20:33:39 +0900

    송재영/0518/상품 리스트, 상품 상세보기

--- src/test/java/com/neighbor/market/NeighborApplicationTests.java
+++ src/test/java/com/neighbor/market/NeighborApplicationTests.java
@@ -1,5 +1,7 @@
 package com.neighbor.market;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
