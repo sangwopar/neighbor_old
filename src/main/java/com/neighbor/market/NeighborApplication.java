9a9b0349c8c2f364c4a6b4a2affac15f2df2b0cf
song <jefkdjsf2@naver.com>
2023-05-19 16:25:56 +0900

    송재영/0519/테스트

--- src/main/java/com/neighbor/market/NeighborApplication.java
+++ src/main/java/com/neighbor/market/NeighborApplication.java
@@ -1,13 +1,32 @@
 package com.neighbor.market;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan("com.neighbor.market.dao")
 public class NeighborApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborApplication.class, args);
+	}
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource)
+			throws Exception{
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		return bean.getObject();
+	}
+	@Bean
+	public SqlSessionTemplate sqlSession(SqlSessionFactory factory){
+		return new SqlSessionTemplate(factory);
 	}
 
 }
